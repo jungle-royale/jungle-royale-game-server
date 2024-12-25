76ac4057317f9476a2dd6775c5fcaed6a2c473ea
joohyuk99 <joohyuk99@gmail.com>
2024-12-25 22:45:27 +0900

    feat: 코어 로직 작성(player 이동 수신, 상태 계산 루프, 상태 브로드캐스트

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module jungle-royale
 go 1.23.4
 
 require (
-	github.com/gorilla/websocket v1.5.3 // indirect
-	google.golang.org/protobuf v1.36.0 // indirect
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/websocket v1.5.3
+	google.golang.org/protobuf v1.36.0
 )
--- object/player.go
+++ object/player.go
@@ -1,6 +1,12 @@
 package object
 
+import (
+	"jungle-royale/message"
+	"sync"
+)
+
 type Player struct {
+	lock    sync.Mutex
 	id      string
 	x       float32
 	y       float32
@@ -10,15 +16,27 @@ type Player struct {
 }
 
 func NewPlayer(id string, x float32, y float32) *Player {
-	return &Player{id, x, y, 30, 0, 0}
+	return &Player{sync.Mutex{}, id, x, y, 30, 0, 0}
 }
 
 func (player *Player) Move() {
+	player.lock.Lock()
 	player.x += player.dx
 	player.y += player.dy
+	player.lock.Unlock()
 }
 
 func (player *Player) DirChange(dx float32, dy float32) {
+	player.lock.Lock()
 	player.dx = dx
 	player.dy = dy
+	player.lock.Unlock()
+}
+
+func (player *Player) MakeSendingPlayerData() *message.Player {
+	return &message.Player{
+		Id: player.id,
+		X:  player.x,
+		Y:  player.y,
+	}
 }
