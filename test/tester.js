7962119e3ae63839e4842fcc7abccdd59245d787
joohyuk99 <joohyuk99@gmail.com>
2025-01-03 15:22:47 +0900

    feat: 이동 테스트 구현

--- test/tester.js
+++ test/tester.js
@@ -38,4 +38,44 @@ module.exports = {
     // console.log("Sent Protobuf message:", message);
     done();
   },
+
+  upMessage: (context, events, done) => { 
+    const change = new message.ChangeDir();
+    change.setAngle(0)
+    change.setIsmoved(true)
+    wrapper.setChangedir(change)
+    const binaryData = wrapper.serializeBinary();
+    context.ws.send(binaryData);
+    done();
+  },
+
+  leftMessage: (context, events, done) => { 
+    const change = new message.ChangeDir();
+    change.setAngle(270)
+    change.setIsmoved(true)
+    wrapper.setChangedir(change)
+    const binaryData = wrapper.serializeBinary();
+    context.ws.send(binaryData);
+    done();
+  },
+
+  rightMessage: (context, events, done) => { 
+    const change = new message.ChangeDir();
+    change.setAngle(90)
+    change.setIsmoved(true)
+    wrapper.setChangedir(change)
+    const binaryData = wrapper.serializeBinary();
+    context.ws.send(binaryData);
+    done();
+  },
+
+  downMessage: (context, events, done) => { 
+    const change = new message.ChangeDir();
+    change.setAngle(180)
+    change.setIsmoved(true)
+    wrapper.setChangedir(change)
+    const binaryData = wrapper.serializeBinary();
+    context.ws.send(binaryData);
+    done();
+  }
 };
