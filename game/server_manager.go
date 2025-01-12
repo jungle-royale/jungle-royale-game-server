a4c3fb5ab2e0815c51644ff0c9650802d3b618a0
joohyuk99 <joohyuk99@gmail.com>
2025-01-13 08:41:03 +0900

    feat: 게임 확인 api

--- game/server_manager.go
+++ game/server_manager.go
@@ -5,6 +5,7 @@ import (
 	"jungle-royale/cons"
 	"jungle-royale/state"
 	"net/http"
+	"strconv"
 )
 
 type gameState struct {
@@ -29,7 +30,6 @@ func (gm *GameManager) SetServerManager() {
 
 		w.Header().Set("Content-Type", "application/json")
 
-		// HTTP 메서드 확인 (POST만 허용)
 		if r.Method != http.MethodGet {
 			http.Error(w, `{"status":405,"message":"Method Not Allowed"}`, http.StatusMethodNotAllowed)
 			return
@@ -61,6 +61,52 @@ func (gm *GameManager) SetServerManager() {
 		fmt.Fprintln(w, "Playing Game List...")
 		for _, gs := range gameStateList {
 			res := gs.toString()
+			fmt.Fprintln(w, res)
+		}
+	})
+
+	// check game state
+	http.HandleFunc("/manage/check-game", func(w http.ResponseWriter, r *http.Request) {
+
+		gameIdxString := r.URL.Query().Get("gameIdx")
+		if gameIdxString == "" {
+			http.Error(w, "Missing gameIdx query parameter", http.StatusBadRequest)
+			return
+		}
+
+		gm.gameManagerLogger.Log("monitoring: check game state " + gameIdxString)
+
+		gameIdx, ok := strconv.Atoi(gameIdxString)
+		if ok != nil {
+			http.Error(w, "gameIdx parameter is invalid", http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		if r.Method != http.MethodGet {
+			http.Error(w, `{"status":405,"message":"Method Not Allowed"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		var gameState gameState
+		game := gm.gameRooms[gameIdx]
+
+		var gameStatus string
+		if game.playerNum != 0 {
+			if game.state.GameState == state.Waiting || game.state.GameState == state.Counting {
+				gameStatus = "Waiting"
+			} else if game.state.GameState == state.Playing {
+				gameStatus = "Playing"
+			}
+			gameState.GameIdx = gameIdx
+			gameState.GameName = game.gameLogger.GetGameName()
+			gameState.GameStatus = gameStatus
+			gameState.PlayerNum = game.state.Players.Length()
+			gameState.LastSec = (game.state.LastGameTick*16)/1000 + cons.WAITING_MAP_CHUNK_NUM
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			fmt.Fprintf(w, "Game %d state...\n", gameIdx)
+			res := gameState.toString()
 			fmt.Fprintln(w, res)
 		}
 	})
